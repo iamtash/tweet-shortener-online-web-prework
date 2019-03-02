7ce54bf4f2ad1bf4c6f9c5fb05d0c3ba1f1d166c
Natasha Chernis <natashachernis@gmail.com>
2019-03-02 20:09:25 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+
+  dictionary
+end
+
+
+def word_substituter(tweet)
+  binding.pry
+
+  dictionary.keys.each do |key|
+    if key == tweet.split(" ").map {|word| word}
+      dictionary[key]
+    else
+      word
+    end
+  end
+end
